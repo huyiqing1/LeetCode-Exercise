e7cb0c734bdf765c077c6dfe7905b81127ca63b6
huyiqing1 <huyiqing1@gmail.com>
2022-02-16 17:10:04 -0500

    commit

---
+++ 1.two-sum.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+
+/*
+ * @lc app=leetcode id=1 lang=java
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] res = new int[2];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                res[0] = i;
+                res[1] = map.get(target - nums[i]);
+                return res;
+            }
+            map.put(nums[i], i);
+        }
+        return res;
+    }
+}
+// @lc code=end
