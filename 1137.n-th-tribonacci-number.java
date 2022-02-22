4c8e74abc74482648418d53c146b1f9bc339f39e
huyiqing1 <huyiqing1@gmail.com>
2022-02-21 22:06:10 -0500

    commit

---
+++ 1137.n-th-tribonacci-number.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=1137 lang=java
+ *
+ * [1137] N-th Tribonacci Number
+ */
+
+// @lc code=start
+class Solution {
+    public int tribonacci(int n) {
+        if (n == 0)
+            return 0;
+        if (n == 1 || n == 2)
+            return 1;
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+        for (int i = 3; i < n + 1; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        }
+        return dp[n];
+    }
+}
+// @lc code=end
+
