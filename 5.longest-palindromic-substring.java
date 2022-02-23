a44ff1704fc1a273054270efb1c4fe8f4ac0cd01
huyiqing1 <huyiqing1@gmail.com>
2022-02-23 16:57:14 -0500

    commit

---
+++ 5.longest-palindromic-substring.java
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode id=5 lang=java
+ *
+ * [5] Longest Palindromic Substring
+ */
+
+// @lc code=start
+class Solution {
+    public String longestPalindrome(String s) {
+        if (s.length() < 1)
+            return "";
+
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int length1 = helper(i, i, s);
+            int length2 = helper(i, i + 1, s);
+            int length = Math.max(length1, length2);
+            if (length > end - start) {
+                start = i - (length - 1) / 2;
+                end = i + length / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private int helper(int left, int right, String s) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+}
+// @lc code=end
