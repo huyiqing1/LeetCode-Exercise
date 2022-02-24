91ffae0f8103afb7047814aff8139070c7ab4d8b
huyiqing1 <huyiqing1@gmail.com>
2022-02-24 13:41:49 -0500

    commit

---
+++ 8.string-to-integer-atoi.java
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode id=8 lang=java
+ *
+ * [8] String to Integer (atoi)
+ */
+
+// @lc code=start
+class Solution {
+    public int myAtoi(String s) {
+        s = s.trim();
+        if (s.length() == 0)
+            return 0;
+        long res = 0;
+        boolean negative = false;
+        if (s.charAt(0) == '-') {
+            negative = true;
+            s = s.substring(1);
+        } else if (s.charAt(0) == '+') {
+            s = s.substring(1);
+        }
+        while (s.length() > 0 && isDigit(s.charAt(0)) && res <= Integer.MAX_VALUE && res >= Integer.MIN_VALUE) {
+            res = res * 10 + s.charAt(0) - 48;
+            s = s.substring(1);
+        }
+        if (negative)
+            res *= -1;
+        if (res >= Integer.MAX_VALUE)
+            return Integer.MAX_VALUE;
+        if (res <= Integer.MIN_VALUE)
+            return Integer.MIN_VALUE;
+        return (int) res;
+    }
+
+    private boolean isDigit(char c) {
+        return c >= '0' && c <= '9';
+    }
+}
+// @lc code=end
