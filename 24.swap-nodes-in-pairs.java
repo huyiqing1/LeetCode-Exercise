cb2eb31709f782e686319d74671abb01ec9fe6e0
huyiqing1 <huyiqing1@gmail.com>
2022-02-25 14:08:54 -0500

    commit

---
+++ 24.swap-nodes-in-pairs.java
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=24 lang=java
+ *
+ * [24] Swap Nodes in Pairs
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        ListNode temp = head.next;
+        ListNode tempHead = swapPairs(head.next.next);
+        head.next = tempHead;
+        temp.next = head;
+        return temp;
+    }
+}
+// @lc code=end
