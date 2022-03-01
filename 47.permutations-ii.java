daff524465a3585f56b68fb276c89ee05cfb645a
huyiqing1 <huyiqing1@gmail.com>
2022-03-01 15:27:13 -0500

    commit

--- 47.permutations-ii.java
+++ 47.permutations-ii.java
@@ -7,15 +7,16 @@ import java.util.*;
 
 // @lc code=start
 class Solution {
-    private HashSet<List<Integer>> res = new HashSet<>();
+    private List<List<Integer>> res = new ArrayList<>();
 
     public List<List<Integer>> permuteUnique(int[] nums) {
         if (nums.length == 1) {
             res.add(new ArrayList<>(Arrays.asList(nums[0])));
-            return new ArrayList<>(res);
+            return res;
         }
+        Arrays.sort(nums);
         helper(new ArrayList<>(), nums, new boolean[nums.length]);
-        return new ArrayList<>(res);
+        return res;
     }
 
     private void helper(List<Integer> list, int[] nums, boolean[] visited) {
@@ -23,8 +24,11 @@ class Solution {
             res.add(new ArrayList<>(list));
             return;
         }
+        
         for (int i = 0; i < nums.length; i++) {
             if (visited[i])
+                continue;
+            if (i > 0 && nums[i] == nums[i - 1] && !visited[i - 1])
                 continue;
             list.add(nums[i]);
             visited[i] = true;
