d3ff022cfb853c9a1bdb76b0449ade495ac74e8c
huyiqing1 <huyiqing1@gmail.com>
2022-03-02 16:43:32 -0500

    commit

---
+++ 73.set-matrix-zeroes.java
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=73 lang=java
+ *
+ * [73] Set Matrix Zeroes
+ */
+
+// @lc code=start
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean[][] isZero = new boolean[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0)
+                    isZero[i][j] = true;
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (isZero[i][j]) {
+                    for (int x = 0; x < m; x++)
+                        matrix[x][j] = 0;
+                    for (int y = 0; y < n; y++)
+                        matrix[i][y] = 0;
+                }
+            }
+        }
+    }
+}
+// @lc code=end
+
