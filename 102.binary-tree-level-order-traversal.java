be92b11543276b878e29ddcd6fb0b3e2d416438b
huyiqing1 <huyiqing1@gmail.com>
2022-03-06 19:59:50 -0500

    commit

---
+++ 102.binary-tree-level-order-traversal.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+/*
+ * @lc app=leetcode id=102 lang=java
+ *
+ * [102] Binary Tree Level Order Traversal
+ */
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null)
+            return res;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while (!q.isEmpty()) {
+            int size = q.size();
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode t = q.poll();
+                list.add(t.val);
+                if (t.left != null)
+                    q.offer(t.left);
+                if (t.right != null)
+                    q.offer(t.right);
+            }
+            res.add(list);
+        }
+        return res;
+    }
+}
+// @lc code=end
