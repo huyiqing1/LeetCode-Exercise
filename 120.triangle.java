d11775c9682258fe22c6952ddfd1350ea2148a41
huyiqing1 <huyiqing1@gmail.com>
2022-03-07 14:36:12 -0500

    commit

---
+++ 120.triangle.java
@@ -0,0 +1,21 @@
+import java.util.*;
+/*
+ * @lc app=leetcode id=120 lang=java
+ *
+ * [120] Triangle
+ */
+
+// @lc code=start
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        for (int i = triangle.size() - 2; i >= 0; i--) {
+            for (int j = 0; j < triangle.get(i).size(); j++) {
+                int temp = triangle.get(i).get(j)
+                        + Math.min(triangle.get(i + 1).get(j), triangle.get(i + 1).get(j + 1));
+                triangle.get(i).set(j, temp);
+            }
+        }
+        return triangle.get(0).get(0);
+    }
+}
+// @lc code=end
